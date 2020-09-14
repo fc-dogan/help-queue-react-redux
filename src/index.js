a51955ddcdb80fea5b9f68443b305fdc386f3a20
Fatma <fceyda@gmail.com>
2020-09-14 10:04:50 -0700

    configure the React-Redux Provider

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers/ticket-list-reducer';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer);
+// we are passing our reducer into the createStore function. That means the store constant is a Redux store that knows how to handle the actions we've defined in our reducer.
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 
 
