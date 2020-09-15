31c46410e44d4dd5f1957b012ce81dc5938c87bc
Fatma <fceyda@gmail.com>
2020-09-15 09:39:04 -0700

    pass the root reducer into store in src/index.js

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -20,7 +20,7 @@ describe('rootReducer', () => {
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
-  
+
   test('Check that initial state of ticketListReducer matches root reducer', () => {
     const action = {
       type: 'ADD_TICKET',
@@ -40,5 +40,6 @@ describe('rootReducer', () => {
     store.dispatch(action);
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
   });
+  
   
 });
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
-import reducer from './reducers/ticket-list-reducer';
+import rootReducer from '/reducers/index';
 import { Provider } from 'react-redux';
 
 const store = createStore(
-  reducer,
+  rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 // we are passing our reducer into the createStore function. That means the store constant is a Redux store that knows how to handle the actions we've defined in our reducer.
