f4f1b4540ecd1d96c64f61cb1aa71b4b49f2b4f7
Fatma <fceyda@gmail.com>
2020-09-14 17:36:40 -0700

    pass tests to ensure that our root reducer is returning the default state of each individual reducer

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -1,11 +1,24 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import ticketListReducer from '../../reducers/ticket-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe('rootReducer', () => {
-  test('should returndefaultstate if no action type is recognized', () => {
+  test('should return default state if no action type is recognized', () => {
     expect(rootReducer({}, {type: null})).toEqual({
       masterTicketList: {},
       formVisibleOnPage: false
     });
+  });
+
+  test('check that initial state of ticketListReducer matches root reducer', () => {
+    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, {type:null}));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
   
 });
