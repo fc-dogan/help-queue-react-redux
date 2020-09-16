60cc4e386f20fef590daff4f48e692bda1750082
Fatma <fceyda@gmail.com>
2020-09-16 09:34:00 -0700

    pass teh test for addTicket action

--- src/_tests_/actions/index.test.js
+++ src/_tests_/actions/index.test.js
@@ -1,6 +1,6 @@
 import * as actions from './../../actions';
 
-describe('help que actions', () => {
+describe('help queue actions', () => {
   it('deleteTicket should create DELETE_TICKET action', () => {
     expect(actions.deleteTicket(1)).toEqual({
       type: 'DELETE_TICKET',
@@ -8,9 +8,19 @@ describe('help que actions', () => {
     });
   });
 
-  it('toggleForm shoul create TOGGLE_FORM action', () => {
+  it('toggleForm should create TOGGLE_FORM action', () => {
     expect(actions.toggleForm()).toEqual({
       type: 'TOGGLE_FORM'
+    });
+  });
+
+  it('addTicket should create ADD_TICKET action', () => {
+    expect(actions.addTicket({names: 'Jo and Jasmine', location: '3E', issue: 'Redux not working!', id: 1})).toEqual({
+      type: 'ADD_TICKET',
+      names: 'Jo and Jasmine',
+      location: '3E',
+      issue: 'Redux not working!',
+      id: 1
     });
   });
 });
